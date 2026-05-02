32fedc1c35cf5558a6d1365083c71c779977be57
João Victor <elietejvc@hotmail.com>
2026-05-02 02:57:04 -0300

    fix: build image

--- Kureimo.API/Program.cs
+++ Kureimo.API/Program.cs
@@ -111,7 +111,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
+if (app.Environment.IsDevelopment())
+    app.UseHttpsRedirection();
 
 app.UseRateLimiter();
 
