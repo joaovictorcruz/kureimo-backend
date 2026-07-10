ae398b3c252c1f55d35569e5688b3387af257913
joaovictorcruz <elietejvc@hotmail.com>
2026-07-10 04:44:07 -0300

    chore: new Cors url path

--- Kureimo.API/Program.cs
+++ Kureimo.API/Program.cs
@@ -82,7 +82,7 @@ builder.Services.AddCors(options =>
     {
         policy
             .WithOrigins(
-                "https://kureimo-production.up.railway.app",
+                "https://kureimo.com",
                 "https://kureimohml.vercel.app", // Dominio homolog
                 "http://localhost:5173"  // Vite dev server padrão
             )
